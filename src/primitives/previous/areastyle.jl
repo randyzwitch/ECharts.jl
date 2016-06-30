26a58e7a60480596261907d973924a09ba060054
randyzwitch <rzwitch@gmail.com>
2016-06-30 09:51:06 -0400

    Remove previous files

--- src/primitives/previous/areastyle.jl
+++
@@ -1,7 +0,0 @@
-area_spec =
-[
-	(:color, AbstractString, nothing),
-	(:_type, AbstractString, "default")
-]
-
-primitivefactory(:AreaStyle, area_spec)
--- src/primitives/previous/splitarea.jl
+++
@@ -1,8 +0,0 @@
-splitarea_spec =
-[
-	(:show, Bool, true),
-	(:onGap, Bool, nothing),
-	(:areaStyle, AreaStyle, nothing),
-]
-
-primitivefactory(:SplitArea, splitarea_spec)
