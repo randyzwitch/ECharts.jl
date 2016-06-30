cdc6576a670ec14c8ab57f332866077b46c11166
randyzwitch <rzwitch@gmail.com>
2016-06-30 10:08:24 -0400

    Revert "Merge pull request #4 from randyzwitch/exhaustive-types"
    This reverts commit 582aa1cbb4b8efb46aa717ada918978493619bb5, reversing
    changes made to a36bb41d72b31d1a1a2099988f026fffa8ccdb07.

--- src/primitives/toolbox.jl
+++ src/primitives/toolbox.jl
@@ -1,20 +1,23 @@
 toolbox_spec =
 [
-    (:show, Bool, true),
-    (:orient, AbstractString, "horizontal"),
-    (:itemSize, Number, 15),
-    (:itemGap, Number, 10),
-    (:showTitle, Bool, true),
-    # (:feature, Feature, nothing)
-    # (:iconStyle, IconStyle, nothing)
-    (:zlevel, Int, 0),
-    (:z, Int, 2),
-    (:left, Union{Int, AbstractString}, "auto"),
-    (:top, Union{Int, AbstractString}, "auto"),
-    (:right, Union{Int, AbstractString}, "auto"),
-    (:bottom, Union{Int, AbstractString}, "auto"),
-    (:width, Union{Int, AbstractString}, "auto"),
-    (:height, Union{Int, AbstractString}, "auto"),
+    (:show, Bool, false),
+    # (:zlevel, Int, 0),
+    # (:z, Int, 6),
+    (:orient, AbstractString, "vertical"),
+    (:x, Union{AbstractString, Number}, "right"),
+    (:y, Union{AbstractString, Number}, "center"),
+    # (:backgroundColor, AbstractString, nothing),
+    # (:borderColor, AbstractString, "#ccc"),
+    # (:borderWidth, Number, 0),
+    # (:padding, Union{Number, AbstractVector}, 5),
+    (:itemGap, Number, 15),
+    # (:itemSize, Number, 16),
+    # (:color, AbstractVector, nothing),
+    # (:disableColor, AbstractString, "#ddd"),
+    # (:effectiveColor, AbstractString, "red"),
+    # (:showTitle, Bool, true),
+    # (:textStyle, TextStyle, nothing),
+    (:feature, Dict{Any, Any}, Dict{Any, Any}())
 ]
 
 primitivefactory(:Toolbox, toolbox_spec)
