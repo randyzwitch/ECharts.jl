18b2fdf323d8b88c2c32fb0f0919f9992f72b12a
randyzwitch <rzwitch@gmail.com>
2016-08-23 15:30:01 -0400

    Refactor tojs
    Remove macro expansion, replace with abstract type

--- src/ECharts.jl
+++ src/ECharts.jl
@@ -19,7 +19,6 @@ module ECharts
 
 	#Primitives - in order of descending dependency within files
 	include("primitives/definetypes.jl")
-	include("primitives/tojs.jl")
 	include("render.jl")
 
 	#Visualization mutating functions
@@ -31,5 +30,32 @@ module ECharts
 	include("plots/circular_plot.jl")
 	include("plots/funnel_plot.jl")
 	include("plots/radar_plot.jl")
+
+	# From JMW originally
+	tojs(s::Symbol) = string(s)
+	tojs(v::Vector) = [tojs(v_i) for v_i in v]
+
+	function tojs(v::Dict)
+	    res = Dict()
+	    for (k, v) in v
+	        res[k] = tojs(v)
+	    end
+	    return res
+	end
+	tojs(x::Any) = x
+
+	# By convention, using single underscore at beginning to get around reserved words
+	# Check for this, strip off before creating js
+	function tojs(x::AbstractEChartType)
+	    res = Dict()
+
+	    for f in fieldnames(x)
+	        if getfield(x, f) != nothing
+	            startswith(string(f), "_") ? res[string(f)[2:end]] = tojs(getfield(x, f)):
+	                                         res[string(f)] = tojs(getfield(x, f))
+	        end
+	    end
+	    return res
+	end
 
 end # module
