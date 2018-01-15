c35b7eb24c891434dc307523bed42329f5b11ed1
randyzwitch <rzwitch@gmail.com>
2018-01-15 18:09:39 -0500

    Disable weakrefstrings

--- test/runtests.jl
+++ test/runtests.jl
@@ -268,7 +268,7 @@ b = radialbar(x, hcat(0.95 .* y, 1.25 .* y,y), stack = [1,1,2])
 @test typeof(b) == EChart
 
 #18: streamgraph
-s_df = CSV.read(joinpath(dirname(@__FILE__), "..", "exampledata/streamdata.csv"), types = [String, Float64, String])
+s_df = CSV.read(joinpath(dirname(@__FILE__), "..", "exampledata/streamdata.csv"), types = [String, Float64, String], weakrefstrings = false)
 sg = streamgraph(s_df[:date], s_df[:value], s_df[:key], legend = true)
 @test typeof(sg) == EChart
 
