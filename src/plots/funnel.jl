0827790ec81459872c0f28f20eb0cc7dc72d821d
randyzwitch <rzwitch@gmail.com>
2018-02-05 10:24:15 -0500

    Funnel and gauge

--- src/plots/funnel.jl
+++ src/plots/funnel.jl
@@ -1,35 +1,26 @@
 """
-    area(x, y)
+    funnel(names, values)
 
-Creates an `EChart` where region below plotted line filled with color.
+Creates an `EChart` funnel.
 
 ## Methods
 ```julia
-area(x::AbstractVector, y::AbstractVector{<:Union{Missing, Int, AbstractFloat, Rational}})
-area(x::AbstractVector, y::AbstractArray{<:Union{Missing, Int, AbstractFloat, Rational},2})
-area(df::AbstractDataFrame, x::Symbol, y::Symbol)
-area(df::AbstractDataFrame, x::Symbol, y::Symbol, group::Symbol)
-area(k::KernelDensity.UnivariateKDE)
+funnel(names::AbstractVector, values::AbstractVector{<:Union{Missing, Int, AbstractFloat, Rational}})
 ```
 
 ## Arguments
-* `mark::Union{String, AbstractVector} = "line"` : how to display plotted points
-* `fill::Union{Bool, AbstractVector} = true` : fill area below marks with color?
-* `stack::Union{Bool, AbstractVector, Void} = nothing` : stack (add together) when multple series present?
-* `step::Union{String, Void} = nothing` : one of {"start", "end", "middle", nothing}
 * `legend::Bool` : display legend?
-* `scale::Bool = false` : show full Y-axis or truncated
 * `kwargs` : varargs to set any field of resulting `EChart` struct
 
 ## Notes
 
-Reasonable defaults set for different methods of `area`, such as displaying a legend when two or more series present.
+Given the concept of a funnel (largest at top, narrowing), if you pass in the values out of order, the echarts.js library automatically sorts the numbers highest to lowest.
 
 ## Examples
 ```julia
-x = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-y = [11, 11, 15, 13, 12, 13, 10]
-ar = area(x, y)
+v = [100,60,80,70,50]
+n = ["A", "B", "C", "D", "E"]
+fn = funnel(n, v)
 ```
 """
 function funnel(names::AbstractVector, values::AbstractVector{<:Union{Missing, Int, AbstractFloat, Rational}};
