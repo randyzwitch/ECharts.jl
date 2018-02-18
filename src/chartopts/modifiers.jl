1df00d41e79fed2b2aa8de3e6567a65470e717b2
randyzwitch <rzwitch@gmail.com>
2018-02-18 14:10:04 -0500

    Beginning of WAI-ARIA compatibility
    Work towards #38
    
    Need to understand what the standard is, enable things by default
    assuming there isn’t a performance concern.

--- src/chartopts/modifiers.jl
+++ src/chartopts/modifiers.jl
@@ -389,3 +389,10 @@ function theme!(ec::EChart, theme::Theme)
 	ec.theme = theme
 	return ec
 end
+
+function aria!(ec::EChart; show::Bool = true, description::String = "", general::Dict = Dict(), series::Dict = Dict(), data::Dict = Dict())
+
+    ec.aria = Aria(show = show, description = description, general = general, series = series, data = data)
+    return ec
+
+end
