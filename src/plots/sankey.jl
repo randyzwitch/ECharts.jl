914af47cc0b819729dda04347683d2e1f39dfeee
Jake Ireland <jakewilliami@icloud.com>
2021-01-28 02:37:59 +1300

    Addition to Sankey method: read from data file (#90)
    * Add method for reading from datafile
    
    * Added Sankey method to read from an input file
    
    Using the second method mentioned above--as per http://sankeymatic.com/build/'s convention--you can have a datafile (call it `input.txt`), where each line has the information
    ```
    SOURCE [AMOUNT] TARGET
    ```
    In this case.  `SOURCE` and `TARGET` are both `String`s, where `AMOUNT` is a `Real`.  For example:
    ```
    Wages [2000] Budget
    Interest [25] Budget
    Budget [500] Taxes
    Budget [450] Housing
    Budget [310] Food
    Budget [205] Transportation
    Budget [400] Health Care
    Budget [160] Other Necessities
    ```
    And simply run
    ```julia
    s = sankey("input.txt")
    ```

--- src/plots/sankey.jl
+++ src/plots/sankey.jl
@@ -1,3 +1,46 @@
+function collect_data_sankey(datafile::String)
+    data = Vector{Tuple{AbstractString, AbstractString, Int}}()
+    
+    open(datafile) do io
+        while ! eof(io)
+            n1, v, n2 = match(r"(.*)\s+\[(\d+)\]\s+(.*)", readline(io)).captures
+            v = parse(Int, v)
+            push!(data, (n1, n2, v))
+        end
+    end
+    
+    return data
+end
+
+function get_names_sankey(data::Vector{Tuple{AbstractString, AbstractString, Int}})
+    names = Vector{AbstractString}()
+    
+    for line in data
+        n1, n2, v = line
+        push!(names, n1); push!(names, n2)
+    end
+    
+    return unique(names)
+end
+
+function parse_sankey(datafile::String)
+    data = collect_data_sankey(datafile)
+    names = get_names_sankey(data)
+    
+    source = Vector{Union{Missing, Int}}()
+    target = Vector{Union{Missing, Int}}()
+    values = Vector{Union{Missing, Real}}()
+    
+    for line in data
+        n1, n2, v = line
+        push!(values, v)
+        push!(source, findfirst(==(n1), names) - 1)
+        push!(target, findfirst(==(n2), names) - 1)
+    end
+    
+    return names, source, target, values
+end
+
 """
     sankey(names, source, target, value)
 
@@ -7,6 +50,7 @@ Creates an `EChart` diagram displaying the path and frequencies between nodes.
 ```julia
 sankey(names::AbstractVector, source::AbstractVector{<:Union{Missing, Int}},
     target::AbstractVector{<:Union{Missing, Int}}, value::AbstractVector{<:Union{Missing, Real}};
+sankey(datafile::AbstractString)
 ```
 
 ## Arguments
@@ -23,6 +67,26 @@ target = [1, 2, 3, 4, 5, 2, 4, 9, 9]
 value = [124.729, 0.597, 26.862, 280.322, 81.144, 35, 35, 11.606, 63.965]
 s = sankey(name, source, target, value)
 ```
+
+Using the second method mentioned above--as per http://sankeymatic.com/build/'s convention--you can have a datafile (call it `input.txt`), where each line has the information
+```
+SOURCE [AMOUNT] TARGET
+```
+In this case.  `SOURCE` and `TARGET` are both `String`s, where `AMOUNT` is a `Real`.  For example:
+```
+Wages [2000] Budget
+Interest [25] Budget
+Budget [500] Taxes
+Budget [450] Housing
+Budget [310] Food
+Budget [205] Transportation
+Budget [400] Health Care
+Budget [160] Other Necessities
+```
+And simply run
+```julia
+s = sankey("input.txt")
+```
 """
 function sankey(names::AbstractVector,
                 source::AbstractVector{<:Union{Missing, Int}},
@@ -37,3 +101,5 @@ function sankey(names::AbstractVector,
     return ec
 
 end
+
+sankey(datafile::AbstractString; kwargs...) = sankey(parse_sankey(datafile)...; kwargs...)
