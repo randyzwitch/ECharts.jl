c6c9d7ac251a8324d80e3f6187acaa0a543f6a3b
Randy Zwitch <rzwitch@gmail.com>
2026-03-24 10:35:27 -0400

    Support rendering to VSCode plot window

--- src/render.jl
+++ src/render.jl
@@ -1,55 +1,35 @@
 #Pretty-print
 print(x::EChart) = print(json(makevalidjson(x)))
 
-#WebIO Scope/dom
-function createscope(ec::EChart)
-    deps = [
-        "echarts" => joinpath(@__DIR__, "..", "docs", "js", "echarts.min.js")
-    ]
+#Shared helper: produces self-contained HTML with echarts.min.js inlined
+function _echarts_html(ec::EChart)
+    echarts_js = read(joinpath(@__DIR__, "..", "docs", "js", "echarts.min.js"), String)
+    option = json(makevalidjson(ec))
+    theme = json(makevalidjson(ec.theme))
+    chart_id = "echarts_" * string(rand(UInt32))
 
-    # set up web io scope. We need to tell it about our dependencies and then
-    # create a div for our chart.
-    scope = Scope(imports=deps)
-    scope.dom = dom"div#myplot"(
-        style=Dict(:width => "$(ec.ec_width)px", :height => "$(ec.ec_height)px")
-    )
-
-    # extract some echarts stuff
-    option = makevalidjson(ec)
-    renderer = Dict(:renderer => ec.ec_renderer)
-    theme = makevalidjson(ec.theme)
-
-    # teach WebIO how to render the chart, once the `echarts` js lib is loaded
-    onimport(scope, JSExpr.@js function (echarts)
-        @var myChart = echarts.init(this.dom.querySelector("#myplot"), $theme, $renderer)
-        myChart.setOption($option);
-        window.onresize = () -> myChart.resize()
-    end)
-
-    return scope
-end
-
-#Makes EChart into a DOM node
-function WebIO.render(ec::EChart)
-
-    scope = createscope(ec)
-    WebIO.render(scope)
-
+    return """
+    <div id="$(chart_id)" style="width:$(ec.ec_width)px;height:$(ec.ec_height)px;"></div>
+    <script>
+    $(echarts_js)
+    (function() {
+        var dom = document.getElementById('$(chart_id)');
+        var myChart = echarts.init(dom, $(theme), {renderer: '$(ec.ec_renderer)'});
+        myChart.setOption($(option));
+        window.addEventListener('resize', function() { myChart.resize(); });
+    })();
+    </script>
+    """
 end
 
 #Jupyter notebook method
-#doesn't work with JupyterLab unless extension installed (not verified)
-Base.show(io::IO, mm::MIME"text/html", ec::EChart) = Base.show(io, mm, WebIO.render(ec))
+Base.show(io::IO, ::MIME"text/html", ec::EChart) = write(io, _echarts_html(ec))
 
 #REPL method
 function Base.display(::REPL.REPLDisplay, ec::EChart)
-
-    scope = createscope(ec)
     w = Blink.Window()
-    Blink.body!(w, scope)
-
+    Blink.loadhtml!(w, _echarts_html(ec))
 end
 
-#Juno method
-
-#VSCode method
+#VSCode method - displays chart inline in the VSCode plot panel (requires Julia VSCode extension)
+Base.show(io::IO, ::MIME"juliavscode/html", ec::EChart) = write(io, _echarts_html(ec))
